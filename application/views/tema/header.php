bc4467fc67d5908697d6ef11bc2629dc0684e069
Camila Bornis <camila.bornis@gmail.com>
2020-01-15 11:16:52 -0300

    bp cambios - vista de seleccion personal

--- application/views/tema/header.php
+++ application/views/tema/header.php
@@ -16,7 +16,7 @@
 
 <link href="<?php echo base_url();?>assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
 
-<!--link rel="stylesheet" href="<?php //echo base_url();?>assets/css/fullcalendar.css" /--> 
+<!--link rel="stylesheet" href="<?php //echo base_url();?>assets/css/fullcalendar.css" /-->
 <?php header('Content-Type: text/html; charset=UTF-8'); ?>
 <!--<link href="<?php //   echo base_url();?>assets/css/css.css" rel="stylesheet" type="text/css"/>-->
 <!--<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>-->
@@ -31,26 +31,26 @@
 <div id="header">
   <h1><a href="">Bingo Oasis</a></h1>
 </div>
-<!--close-Header-part--> 
+<!--close-Header-part-->
 
 <!--top-Header-menu-->
 <div id="user-nav" class="navbar navbar-inverse">
   <ul class="nav">
-   
+
     <li class=""><a title="" href="<?php echo site_url();?>/bingoOasis/miCuenta"><i class="icon icon-star"></i> <span class="text">Mi cuenta</span></a></li>
-    
+
     <li class=""><a title="" href="<?php echo site_url();?>/bingoOasis/salir"><i class="icon icon-off"></i> <span class="text">Salir del Sistema</span></a></li>
-    
+
   </ul>
 
 </div>
 
 <!--start-top-serch-->
 <div id="search">
-    
+
     <a href="#" ><i class="icon icon-asterisk"></i> <span class="text">Usuario: <?php  echo $this->session->userdata('nombre'); ?></span></a>
 </div>
-<!--close-top-serch--> 
+<!--close-top-serch-->
 
 <!--sidebar-menu-->
 
@@ -60,12 +60,12 @@
 
     <li class="<?php if(isset($menuPainel)){echo 'active';};?>"><a href="<?php echo base_url()?>">
         <i class="icon icon-home"></i> <span>Inicio</span></a></li>
-    
+
     <!--RRHH-->
-    <?php if( $this->permission->checkPermission($this->session->userdata('permiso'),'vPersonas') || 
-//               $this->permission->checkPermission($this->session->userdata('permiso'),'vLicencias') || 
-               $this->permission->checkPermission($this->session->userdata('permiso'),'vCapacitacion') || 
-               $this->permission->checkPermission($this->session->userdata('permiso'),'vPremios')  
+    <?php if( $this->permission->checkPermission($this->session->userdata('permiso'),'vPersonas') ||
+//               $this->permission->checkPermission($this->session->userdata('permiso'),'vLicencias') ||
+               $this->permission->checkPermission($this->session->userdata('permiso'),'vCapacitacion') ||
+               $this->permission->checkPermission($this->session->userdata('permiso'),'vPremios')
             ){ ?>
         <li class="submenu <?php if(isset($menuConfiguracoes)){echo 'active open';};?>">
           <a href="#"><i class="icon icon-list-alt"></i> <span>Recursos Humanos</span> <span class="label"><i class="icon-chevron-down"></i></span></a>
@@ -98,65 +98,65 @@
           </ul>
         </li>
     <?php } ?>
-    
-    
+
+
     <!--GASTRONOMIA-->
     <?php // if($this->permission->checkPermission($this->session->userdata('permiso'),'vMenuPersonal')){ ?>
         <li class="submenu <?php if(isset($menuClientes)){echo 'active open';};?>">
             <?php  if(
-                    //$this->permission->checkPermission($this->session->userdata('permiso'),'vMenu') || 
-                        $this->permission->checkPermission($this->session->userdata('permiso'),'cPedido') || 
-                        $this->permission->checkPermission($this->session->userdata('permiso'),'vPedido') 
+                    //$this->permission->checkPermission($this->session->userdata('permiso'),'vMenu') ||
+                        $this->permission->checkPermission($this->session->userdata('permiso'),'cPedido') ||
+                        $this->permission->checkPermission($this->session->userdata('permiso'),'vPedido')
                     ){ ?>
             <a href="#"><i class="icon  icon-glass"></i> <span>Gastronomia</span><span class="label"><i class="icon-chevron-down"></i></a>
             <?php } ?>
             <ul>
               <?php  //if($this->permission->checkPermission($this->session->userdata('permiso'),'vMenu')){ ?>
                 <li><a href="<?php echo base_url()?>index.php/menuPersonal">ABM Menu</a></li>
-              <?php  //} 
+              <?php  //}
                     if($this->permission->checkPermission($this->session->userdata('permiso'),'vPedido')){ ?>
                 <li><a   href="<?php echo base_url().'index.php/pedido/monitor';?>">Monitor</a></li>
                 <?php }
                 if($this->permission->checkPermission($this->session->userdata('permiso'),'cPedido')){ ?>
                 <li><a   href="<?php echo base_url().'index.php/pedido/pedidos';?>">Pedidos</a></li>
                 <?php } ?>
-          </ul>    
+          </ul>
         </li>
-    <?php // } ?>  
-    
-    
+    <?php // } ?>
+
+
     <!--SALA DE REUNUNIONES-->
     <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vSala')){ ?>
         <li class=" <?php if(isset($menuClientes)){echo 'active open';};?>">
             <a href="<?php echo base_url()?>index.php/sala"><i class="icon icon-calendar"></i> <span>Sala de Reuniones</span></a>
-           
+
         </li>
-    <?php } ?>  
-        
+    <?php } ?>
+
     <!--MAQUINAS-->
     <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vMaquina')){ ?>
         <li class=" <?php if(isset($menuClientes)){echo 'active open';};?>">
             <a href="<?php echo base_url()?>index.php/maquinas"><i class="icon icon-desktop"></i> <span>Maquinas</span></a>
-           
+
         </li>
-    <?php } ?>  
-        
+    <?php } ?>
+
     <!--FALLAS-->
     <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vFallas')){ ?>
         <li class=" <?php if(isset($menuClientes)){echo 'active open';};?>">
             <a href="<?php echo base_url()?>index.php/fallas"><i class="icon icon-warning-sign"></i> <span>Fallas</span></a>
         </li>
-    <?php } ?>        
-        
+    <?php } ?>
+
     <!--LABORATORIO-->
     <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vLaboratorio')){ ?>
         <li class=" <?php if(isset($menuClientes)){echo 'active open';};?>">
             <a href="<?php echo base_url()?>index.php/laboratorio"><i class="icon icon icon-wrench "></i> <span>Laboratorio</span></a>
         </li>
-    <?php } ?>        
-    
+    <?php } ?>
+
     <!--REPORTES-->
-    <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vRep_maquinas')  || 
+    <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vRep_maquinas')  ||
             $this->permission->checkPermission($this->session->userdata('permiso'),'vReportes') ){ ?>
         <li class="submenu <?php if(isset($menuConfiguracoes)){echo 'active open';};?>">
           <a href="#"><i class="icon icon-list-alt"></i> <span>Reportes</span> <span class="label"><i class="icon-chevron-down"></i></span></a>
@@ -170,14 +170,16 @@
               <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vRep_pedido')){ ?>
                 <li><a href="<?php echo base_url()?>index.php/Reportes/pedidos">Pedidos</a></li>
                 <li><a href="<?php echo base_url()?>index.php/Reportes/calendariomenu">Menus programados</a></li>
+                <li><a href="<?php echo base_url()?>index.php/Reportes/seleccionPersonal">Seleccion Personal</a></li>
+                
               <?php } ?>
                 <!--<li><a href="#">Fallas (en construccion)</a></li>-->
           </ul>
         </li>
     <?php } ?>
-    
+
     <!--INVENTARIO-->
-    <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vArticulos')  || 
+    <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vArticulos')  ||
             $this->permission->checkPermission($this->session->userdata('permiso'),'vCategorias') ){ ?>
         <li class="submenu <?php if(isset($menuConfiguracoes)){echo 'active open';};?>">
           <a href="#"><i class="icon icon-barcode"></i> <span>Inventario</span> <span class="label"><i class="icon-chevron-down"></i></span></a>
@@ -185,20 +187,20 @@
                 <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vArticulos')){ ?>
                     <li><a href="<?php  echo base_url()?>index.php/articulo">Articulos </a></li>
                     <!--<li><a href="#">Articulos (en construccion)</a></li>-->
-                <?php } 
+                <?php }
                    if($this->permission->checkPermission($this->session->userdata('permiso'),'vArticulos')){ ?>
                     <li><a href="<?php  echo base_url()?>index.php/articulo/paniol">Pañol</a></li>
                     <!--<li><a href="#">Articulos (en construccion)</a></li>-->
-                <?php } 
+                <?php }
                 if($this->permission->checkPermission($this->session->userdata('permiso'),'vCategorias')){ ?>
                     <li><a href="#">Categorias (en construccion)</a></li>
                 <?php  }?>
           </ul>
         </li>
     <?php } ?>
-    
+
     <!--AYUDA-->
-    <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vTicket')  || 
+    <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vTicket')  ||
             $this->permission->checkPermission($this->session->userdata('permiso'),'vManuales') ){ ?>
         <li class="submenu <?php if(isset($menuConfiguracoes)){echo 'active open';};?>">
           <a href="#"><i class="icon icon-flag"></i> <span>Ayuda</span> <span class="label"><i class="icon-chevron-down"></i></span></a>
@@ -209,13 +211,13 @@
             <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vManuales')){ ?>
                 <li><a href="#">Manuales (en construccion)</a></li>
             <?php } ?>
- 
+
           </ul>
         </li>
     <?php } ?>
-        
+
     <!--IMPORTADOR-->
-    <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vImporMaquinas')  || 
+    <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vImporMaquinas')  ||
             $this->permission->checkPermission($this->session->userdata('permiso'),'vImpor') ){ ?>
         <li class="submenu <?php if(isset($menuConfiguracoes)){echo 'active open';};?>">
           <a href="#"><i class="icon-upload"></i> <span>Importador</span> <span class="label"><i class="icon-chevron-down"></i></span></a>
@@ -223,23 +225,23 @@
             <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vImporMaquinas')){ ?>
                 <li><a href="<?php echo base_url()?>index.php/importador/maquinas">Maquinas</a></li>
             <?php } ?>
- 
+
             <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vImporArticulos')){ ?>
                 <li><a href="<?php echo base_url()?>index.php/importador/articulos">Articulos</a></li>
             <?php } ?>
- 
+
             <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vImporArticulos_maq')){ ?>
                 <li><a href="<?php echo base_url()?>index.php/importador/articulos_maquinas">Articulos a máquinas</a></li>
             <?php } ?>
- 
+
             <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'vImporMenu')){ ?>
                 <li><a href="<?php echo base_url()?>index.php/importador/menu">Menu</a></li>
             <?php } ?>
- 
+
           </ul>
         </li>
     <?php } ?>
-    
+
     <!--CONFIGURACION-->
     <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'cUsuario')  || $this->permission->checkPermission($this->session->userdata('permiso'),'cPermiso') || $this->permission->checkPermission($this->session->userdata('permiso'),'cBackup')){ ?>
         <li class="submenu <?php if(isset($menuConfiguracoes)){echo 'active open';};?>">
@@ -257,32 +259,32 @@
             <?php if($this->permission->checkPermission($this->session->userdata('permiso'),'cBackup')){ ?>
                 <li><a href="<?php echo base_url()?>index.php/bingoOasis/backup">Backup</a></li>
             <?php } ?>
- 
+
           </ul>
         </li>
     <?php } ?>
-    
-    
+
+
   </ul>
 </div>
 <div id="content">
   <div id="content-header">
-        <div id="breadcrumb" > 
-            <a href="<?php echo base_url()?>" title="Inicio" class="tip-bottom"><i class="icon-home"></i> Inicio</a> 
+        <div id="breadcrumb" >
+            <a href="<?php echo base_url()?>" title="Inicio" class="tip-bottom"><i class="icon-home"></i> Inicio</a>
                     <?php if($this->uri->segment(1) != null){
                                 if($this->uri->segment(1) == 'pedido'){
                                     echo " <a class='tip-bottom'> ".$this->uri->segment(2). "</a>";
                                 }else{ ?>
-                                    <a href="<?php echo base_url().'index.php/'.$this->uri->segment(1)?>" class="tip-bottom" title="<?php echo ucfirst($this->uri->segment(1));?>"><?php echo ucfirst($this->uri->segment(1));?></a> 
+                                    <a href="<?php echo base_url().'index.php/'.$this->uri->segment(1)?>" class="tip-bottom" title="<?php echo ucfirst($this->uri->segment(1));?>"><?php echo ucfirst($this->uri->segment(1));?></a>
                                             <?php if($this->uri->segment(2) != null){?>
-                                    <a href="<?php echo base_url().'index.php/'.$this->uri->segment(1).'/'.$this->uri->segment(2).'/'.$this->uri->segment(3) ?>" class="current tip-bottom" title="<?php echo ucfirst($this->uri->segment(2)); ?>"><?php echo ucfirst($this->uri->segment(2));} ?></a> 
-                                    
+                                    <a href="<?php echo base_url().'index.php/'.$this->uri->segment(1).'/'.$this->uri->segment(2).'/'.$this->uri->segment(3) ?>" class="current tip-bottom" title="<?php echo ucfirst($this->uri->segment(2)); ?>"><?php echo ucfirst($this->uri->segment(2));} ?></a>
+
                               <?php  }?>
-            
+
                     <?php }?>
-            
+
         </div>
-      
+
   </div>
   <div class="container-fluid">
     <div class="row-fluid">
@@ -300,7 +302,7 @@
                               <?php echo $this->session->flashdata('success');?>
                            </div>
                       <?php }?>
-                          
+
                       <?php if(isset($view)){echo $this->load->view($view, null, true);}?>
 
       </div>
@@ -314,8 +316,8 @@
 <!--end-Footer-part-->
 
 
-<script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script> 
-<script src="<?php echo base_url();?>assets/js/matrix.js"></script> 
+<script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url();?>assets/js/matrix.js"></script>
 
 <script language="JavaScript">
 	function FullScreen(theURL)
@@ -327,17 +329,10 @@
             params += ', fullscreen=yes';
 		window.open(theURL, '', params);
 	}
-        
+
 </Script>
- 
+
 
 
 </body>
 </html>
-
-
-
-
-
-
-
